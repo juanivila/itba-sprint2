4c394e7aa9746f25709bd48613ddece6559e151f
Juano <jivila@outlook.com>
2022-06-04 14:16:51 -0300

    Formulario con funcionalidad y maqueta agregado

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-'use strict'
+"use strict";
 
+const data = [];
+const submitBtn = document.querySelector("#btn-submit");
+const nombrePersonaEl = document.querySelector("#nombre-persona");
+const gastosPersonaEl = document.querySelector("#gastos-persona");
 
+const nombrePersonaArr = new Array();
+const gastoPersonaArr = new Array();
+const formEl = document.querySelector("form");
 
+// Cuando carga la pagina
+nombrePersonaEl.focus();
 
+//-------------------------------------------------- Eventos --------------------------------------------------
 
+// Click enviar
+submitBtn.addEventListener("click", e => {
+	e.preventDefault();
 
+	// Agregando persona y cantidad de gastos
+	nombrePersonaArr.push(nombrePersonaEl.value);
+	gastoPersonaArr.push(gastosPersonaEl.value);
+
+	// Cleareando el form y reseteando focus
+	formEl.reset();
+	nombrePersonaEl.focus();
+});
