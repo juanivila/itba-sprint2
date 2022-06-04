0a4ea050e6913f6a362d8e0608b8bcab38cbbda4
Juano <jivila@outlook.com>
2022-06-04 14:47:48 -0300

    Gastos totales agregados / pendiente de correccion

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ const data = [];
 const submitBtn = document.querySelector("#btn-submit");
 const nombrePersonaEl = document.querySelector("#nombre-persona");
 const gastosPersonaEl = document.querySelector("#gastos-persona");
+const formEl = document.querySelector("form");
+const personaAgregada = document.querySelector(".persona-agregada");
+const gastosTotalesEl = document.querySelector('#section--gastos-totales')
 
 const nombrePersonaArr = new Array();
 const gastoPersonaArr = new Array();
-const formEl = document.querySelector("form");
+
+
+
 
 // Cuando carga la pagina
 nombrePersonaEl.focus();
 
 //-------------------------------------------------- Eventos --------------------------------------------------
 
-// Click enviar
+// Clickeando enviar
 submitBtn.addEventListener("click", e => {
 	e.preventDefault();
 
@@ -22,7 +27,14 @@ submitBtn.addEventListener("click", e => {
 	nombrePersonaArr.push(nombrePersonaEl.value);
 	gastoPersonaArr.push(gastosPersonaEl.value);
 
+	// Agregando HTML con la persona
+	personaAgregada.textContent = `Los gastos de ${nombrePersonaEl.value} por $${gastosPersonaEl.value} fueron agregados correctamente.`;
+    
+	// Agregando a gastos totales
+    gastosTotalesEl.after(`${nombrePersonaEl.value}: ${gastosPersonaEl.value}`)
+    
 	// Cleareando el form y reseteando focus
 	formEl.reset();
 	nombrePersonaEl.focus();
+
 });
